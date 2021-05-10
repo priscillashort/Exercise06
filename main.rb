44731869903a17e39f5688516a8b640b37b2f633
priscillashort <pshort2@cocc.edu>
2021-05-09 21:36:40 -0700

    use the point class in all places where x and y were used before

--- main.rb
+++ main.rb
@@ -1,17 +1,17 @@
-def draw_button(label_text, x, y, foreground_color, is_dark_mode)
+def draw_button(label_text, point, foreground_color, is_dark_mode)
     if is_dark_mode
-        darken_foreground(label_text, x, y, foreground_color)
+        darken_foreground(label_text, point, foreground_color)
     else
-        lighten_foreground(label_text, x, y, foreground_color)
+        lighten_foreground(label_text, point, foreground_color)
     end
 end
 
-def darken_foreground(label_text, x, y, foreground_color)
-    paint(label_text, x, y, foreground_color - 10, '#111111')
+def darken_foreground(label_text, point, foreground_color)
+    paint(label_text, point, foreground_color - 10, '#111111')
 end
 
-def lighten_foreground(label_text, x, y, foreground_color)
-    paint(label_text, x, y, foreground_color + 10, '#E0E0E0')
+def lighten_foreground(label_text, point, foreground_color)
+    paint(label_text, point, foreground_color + 10, '#E0E0E0')
 end
 
 class Point
