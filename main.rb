32125218db3b95523a8e2f7757b2ae7f36c9a271
priscillashort <pshort2@cocc.edu>
2021-05-09 21:11:11 -0700

    delete the old code and the comment

--- main.rb
+++ main.rb
@@ -2,8 +2,6 @@ def draw_button(label_text, x, y, foreground_color, is_dark_mode)
     if is_dark_mode
         darken_foreground(label_text, x, y, foreground_color)
     else
-        # lighten foreground color for non-dark mode
-        paint(label_text, x, y, foreground_color + 10, '#E0E0E0')
         lighten_foreground(label_text, x, y, foreground_color)
     end
 end
