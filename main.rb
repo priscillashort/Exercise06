7112f43bd4762cf09fef82ef050ca1af70878335
priscillashort <pshort2@cocc.edu>
2021-05-09 21:06:57 -0700

    call the darken foreground function and return it's value instead, whle maintaining the old code

--- main.rb
+++ main.rb
@@ -2,12 +2,13 @@ def draw_button(label_text, x, y, foreground_color, is_dark_mode)
     if is_dark_mode
         # darken foreground color for dark mode
         paint(label_text, x, y, foreground_color - 10, '#111111')
+        darken_foreground(label_text, x, y, foreground_color)
     else
         # lighten foreground color for non-dark mode
         paint(label_text, x, y, foreground_color + 10, '#E0E0E0')
     end
 end
 
-def darken_foreground
+def darken_foreground(label_text, x, y, foreground_color)
     paint(label_text, x, y, foreground_color - 10, '#111111')
 end
